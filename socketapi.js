dcc8d0b3540666b014d8e35ceaa9bfb9b9e78930
LeVietHa05 <ha.leviet.0504@gmail.com>
2023-12-03 18:48:11 +0700

    update socket server

--- socketapi.js
+++ socketapi.js
@@ -1,4 +1,15 @@
-const io = require("socket.io")();
+
+const option = {
+    allowEIO3: true,
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+        transports: ["websocket", "polling"],
+        credentials: true,
+    },
+}
+
+const io = require("socket.io")(option);
 
 const socketapi = {
     io: io
@@ -7,8 +18,25 @@ const socketapi = {
 io.on("connection", (socket) => {
     console.log("[INFO] new connection: [" + socket.id + "]");
     socket.on("message", (data) => {
-        console.log(`message from ${data.clientID} via socket id: ${socket.id}`);
+        console.log(`message from ${data.clientID} via socket id: ${socket.id} on topic message`);
         socket.broadcast.emit("message", data);
+    });
+
+    socket.on('/esp/measure', (data) => {
+        console.log(`message from ${data.clientID} via socket id: ${socket.id} on topic /esp/measure`);
+        socket.broadcast.emit('/web/measure', data);
+    })
+
+    socket.on("/web/control", (data) => {
+        console.log(`message from ${data.clientID} via socket id: ${socket.id} on topic /web/control`);
+        console.log(data);
+        socket.broadcast.emit("/esp/control", data);
+    });
+
+    socket.on("esp/other", (data) => {
+        console.log(`message from ${data.clientID} via socket id: ${socket.id} on topic esp/other`);
+        socket.broadcast.emit("web/other", data);
+        console.log(data);
     });
     /**************************** */
     //xu ly chung
